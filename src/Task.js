3d944d33d3b65802b5a1a1883770d167126ff11b
Daris Irfan <20082010001@student.upnjatim.ac.id>
2023-06-14 22:52:50 +0700

    add url

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://gorgeous-buttercream-a7230b.netlify.app" // TODO: replace this
 const studentName = "Daris Irfan Putrawanto" // TODO: replace this
 const studentId = "FE5498316" // TODO: replace this
 
